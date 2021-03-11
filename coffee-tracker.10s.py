f441c384e4f2fabec804a1dd625dce450fe6298f
Josh Cook <joshuacook0023@gmail.com>
2021-03-11 09:23:42 -0500

    feat: put request when button clicked

--- coffee-tracker.10s.py
+++ coffee-tracker.10s.py
@@ -1,11 +1,21 @@
 #!//Users/admin/Documents/SwiftBar-Plugins/.env/bin/python3
 
 import argparse
-from typing import Any, Dict, List
+import sys
+from datetime import datetime
+from pathlib import Path
+from typing import Any, Dict, List, Optional
 
+import keyring
 import requests
 
+self_path = Path(sys.argv[0])
+
 api_url = "https://a7a9ck.deta.dev/"
+
+
+def get_api_password() -> Optional[str]:
+    return keyring.get_password("swiftbar_coffee-tracker", "Joshua Cook")
 
 
 class CoffeeBag:
@@ -20,6 +30,9 @@ class CoffeeBag:
 
     def __str__(self) -> str:
         return self.brand + " - " + self.name
+
+
+#### ---- SwiftBar Plugin UI ---- ####
 
 
 def get_active_coffee_bags() -> List[Dict[str, Any]]:
@@ -37,12 +50,12 @@ def get_active_coffee_bags() -> List[Dict[str, Any]]:
 
 def make_click_command(bag: CoffeeBag) -> str:
     cmd = "refresh=true "
-    cmd += f"param0={bag.key}"
+    cmd += f"bash={self_path.as_posix()} "
+    cmd += f"param1='{bag.key}' "
+    cmd += "terminal=false"
     return cmd
 
-
-def put_coffee_use(bag_id: str):
-    print("make request for '" + bag_id + "'")
+    # bash={self_path.as_posix()} param1={env} refresh=true terminal=false
 
 
 def swiftbar_plugin():
@@ -56,11 +69,40 @@ def swiftbar_plugin():
         print(coffee_bag.__str__() + " | " + click_cmd)
 
 
+#### ---- Response to clicking a coffee ---- ####
+
+
+def get_now_formatted() -> str:
+    dtformat = "%Y-%m-%dT%H:%M:%S"
+    return datetime.now().strftime(dtformat)
+
+
+def put_coffee_use(bag_id: str):
+    password = get_api_password()
+    if password is None:
+        raise Exception("Password not found.")
+
+    when = get_now_formatted()
+    url = api_url + f"new_use/{bag_id}?password={password}&when={when}"
+    response = requests.put(url)
+    if response.status_code == 200:
+        print("Successful!")
+        print(response.json())
+    else:
+        print(f"Error: status code {response.status_code}")
+        print(response.json())
+
+
+#### ---- Argument Parsing ---- ####
+
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("bag_id", type=str, default=None, nargs="?")
     return parser.parse_args()
 
+
+#### ---- Main ---- ####
 
 if __name__ == "__main__":
     args = parse_arguments()
