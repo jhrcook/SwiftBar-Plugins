b266164a850e61fbd53b06cf73f1f47b5928e8c7
Josh Cook <joshuacook0023@gmail.com>
2022-01-14 09:17:35 -0500

    style: update env plugin to better Python code

--- python-virtual-environments.1h.py
+++ python-virtual-environments.1h.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/Users/admin/Documents/SwiftBar-Plugins/.env/bin/python3
+
+"""SwiftBar plugin to show all conda environments and copy to clipboard."""
 
 # <bitbar.title>Display Conda Virtual Environments</bitbar.title>
 # <bitbar.version>v1.0</bitbar.version>
 # <bitbar.author>Joshua Cook</bitbar.author>
 # <bitbar.author.github>jhrcook</bitbar.author.github>
-# <bitbar.desc>List the conda virtual environments and copy to clipboard when clicked..</bitbar.desc>
+# <bitbar.desc>List the conda virtual environments and copy to clipboard.</bitbar.desc>
 # <bitbar.image>https://docs.conda.io/en/latest/_images/conda_logo.svg</bitbar.image>
 # <bitbar.dependencies>python3</bitbar.dependencies>
 # <swiftbar.hideAbout>true</swiftbar.hideAbout>
@@ -20,37 +21,41 @@ from pathlib import Path
 self_path = Path(sys.argv[0])
 
 
-def print_environments():
-    """Print the environments to standard output."""
-
-    print("🅒")  # print("🐍 | emojize=true")
-
+def _header() -> None:
+    print(":c.circle: | symbolize=true")
     print("---")
-
     print("Click to copy to clipboard")
+    return None
 
-    conda_envs = subprocess.run(["conda", "env", "list"], stdout=subprocess.PIPE)
-    conda_envs = conda_envs.stdout.decode("utf-8")
-    conda_envs = conda_envs.split("\n")
+
+def print_environments() -> None:
+    """Print the environments to standard output."""
+    _header()
+    conda_envs_subp = subprocess.run(["conda", "env", "list"], stdout=subprocess.PIPE)
+    conda_envs = conda_envs_subp.stdout.decode("utf-8").split("\n")
     for conda_env in conda_envs:
-        if not "#" in conda_env and conda_env != "":
+        if "#" not in conda_env and conda_env != "":
             env = conda_env.strip().split(" ")[0]
             env = Path(env).name
             if env != "base":
-                print(
-                    f"{env} | bash={self_path.as_posix()} param1={env} refresh=true terminal=false"
-                )
+                msg = f"{env} | bash={self_path.as_posix()} param1={env}"
+                msg += " refresh=true terminal=false"
+                print(msg)
 
     print("---")
-
     print("Refresh | refresh=true")
+    return None
 
 
-def copy_env_to_clipboard(env):
+def copy_env_to_clipboard(env: str) -> None:
     """Copy an environment name to clipboard."""
     p1 = subprocess.Popen(["echo", env], stdout=subprocess.PIPE)
     p2 = subprocess.Popen(["pbcopy"], stdin=p1.stdout)
-    p1.stdout.close()
+    if p1.stdout is not None:
+        p1.stdout.close()
+    if p2.stdin is not None:
+        p2.stdin.close()
+    return None
 
 
 if __name__ == "__main__":
